b682988cfa7455e24875dc97e3dd4658331df832
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-01-05 23:08:21 -0800

    added database models and associations

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,10 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then(results => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
